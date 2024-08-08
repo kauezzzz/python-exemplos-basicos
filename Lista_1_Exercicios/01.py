a9cc71bf4ddb3c6add95e929899bfa679439ce46
kaue <130191440+kauezzzz@users.noreply.github.com>
2024-08-08 11:04:09 -0300

    up

---
+++ Lista_1_Exercicios/01.py
@@ -0,0 +1,16 @@
+# Variáveis
+nome = "Luana"
+idade = 17
+altura = 1.63
+peso = 58.0
+cidade = "Piritubacity"
+
+# Exibição de valores
+print(nome)
+print(idade)
+print(altura)
+print(peso)
+print(cidade)
+
+# Método de concatenação (f string)
+print(f"O meu nome é {nome} e tenho {idade} anos. Peso {peso} e tenho {altura}. Moro em {cidade}")
